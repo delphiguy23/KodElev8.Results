99a215c3c13822265f53b034759a0472f5356a1f
delphiguy23 <delphiguy23@gmail.com>
2024-12-29 23:26:54 +0000

    Some Changes

---
+++ Results.Test/Tests.cs
@@ -0,0 +1,255 @@
+using FluentAssertions;
+
+namespace Results.Test
+{
+    public class Tests
+    {
+        [Fact]
+        public void ResultIsSuccessIfThereIsSomething()
+        {
+            var result = 1.ToResults();
+            result.IsSuccess().Should().BeTrue();
+            result.Value.Should().Be(1);
+        }
+
+
+        [Fact]
+        public void ResultIsNullThenItShouldBeNotFound()
+        {
+            string? _results = null;
+            
+            var result = _results.ToResults();
+            result.IsNotFoundOrBadRequest().Should().BeTrue();
+        }
+
+        [Fact]
+        public void ResultExceptionThenFailure()
+        {
+            IResults<int> results = ResultsTo.Something<int>(10);
+            try
+            {
+                var _result = results.Value / 0; //trigger divide by zero exception
+            }
+            catch (Exception ex)
+            {
+                results = ResultsTo.Failure<int>().FromException(ex);
+            }
+
+            results.IsFailure().Should().BeTrue();
+        }
+
+
+        [Fact]
+        public void ResultSuccessThenOk()
+        {
+            IResults<int> AddNumber(int a, int b)
+            {
+                return ResultsTo.Something<int>(a + b);
+            }
+
+            var results = AddNumber(1,2);
+
+            results.IsSuccess().Should().BeTrue();
+            results.Value.Should().Be(3);
+        }
+
+        [Fact]
+        public void ToResultsThenToSomething()
+        {
+            var someValue = 10;
+
+            var result = someValue.ToResults();
+
+            result.IsSuccess().Should().BeTrue();
+            result.Value.Should().BeOfType(typeof(int));
+        }
+
+        [Fact]
+        public void FromSomethingThenToIs()
+        {
+            var someValue = 10;
+
+            var result1 = someValue.ToResults(); 
+            var result2 = result1.Is(x => x > 5);
+
+            result2.IsSuccess().Should().BeTrue();
+            result2.Value.Should().Be(someValue);
+        }
+
+        [Fact]
+        public void WhenSomethingIsResultsToThen()
+        {
+            var someValue = 10;
+
+            var result1 = someValue.ToResults();
+            var result2 = result1.Is<int, int>(x => x > 5, x => ResultsTo.Something(x.Value * 2));
+
+            result2.IsSuccess().Should().BeTrue();
+            result2.Value.Should().Be(someValue*2);
+        }
+
+        [Fact]
+        public void WhenSomethingIsResultsToThenOrElse1()
+        {
+            var someValue = 10;
+
+            var result1 = someValue.ToResults();
+            var result2 = result1.Is<int, int>(
+                x => x > 5, 
+                x => ResultsTo.Something(x.Value * 2),
+                x => ResultsTo.Something(x.Value - 5));
+
+            result2.IsSuccess().Should().BeTrue();
+            result2.Value.Should().Be(someValue * 2);
+        }
+
+        [Fact]
+        public void WhenSomethingIsResultsToThenOrElse2()
+        {
+            var someValue = 5;
+
+            var result1 = someValue.ToResults();
+            var result2 = result1.Is<int, int>(
+                x => x > 5,
+                x => ResultsTo.Something(x.Value * 2),
+                x => ResultsTo.Something(x.Value - 5));
+
+            result2.IsSuccess().Should().BeTrue();
+            result2.Value.Should().Be(someValue - 5);
+        }
+
+        [Fact]
+        public void WhenValidatePassThenSuccess()
+        {
+            var someValue = 10;
+
+            var result1 = someValue.ToResults();
+            var result2 = result1.Validate<int>(x => x < 10,"Value cant be less than 10");
+
+            result2.IsFailure().Should().BeFalse();
+        }
+
+        [Fact]
+        public void WhenValidateFailThenFailure()
+        {
+            var someValue = 9;
+
+            var result1 = someValue.ToResults();
+            var result2 = result1.Validate<int>(x => x < 10, "Value cant be less than 10");
+
+            result2.IsFailure().Should().BeTrue();
+            result2.Messages.Should().Contain("Value cant be less than 10");
+        }
+
+        [Fact]
+        public void WhenNonIResultValidatePassThenSuccess()
+        {
+            var someValue = 10;
+
+            var result2 = someValue.Validate<int>(x => x < 10, "Value cant be less than 10");
+
+            result2.IsFailure().Should().BeFalse();
+        }
+
+        [Fact]
+        public void WhenNonIResultValidateFailThenFailure()
+        {
+            var someValue = 9;
+
+            var result1 = someValue.ToResults();
+            var result2 = result1.Validate<int>(x => x < 10, "Value cant be less than 10");
+
+            result2.IsFailure().Should().BeTrue();
+            result2.Messages.Should().Contain("Value cant be less than 10");
+        }
+
+        [Fact]
+        public void WhenNestedValidatePassThenSuccess10()
+        {
+            var someValue = 20;
+
+            var result2 = someValue
+                            .Validate(x => x < 10, "Value cant be less than 10")
+                            .Validate(x => x < 15, "Value cant be less than 15")
+                            .Validate(x => x < 20, "Value cant be less than 20");
+
+            result2.IsSuccess().Should().BeTrue();
+            result2.Value.Should().Be(someValue);
+        }
+
+        [Theory]
+        [InlineData(10)]
+        [InlineData(15)]
+        [InlineData(20)]
+        public void WhenNestedValidateFailThenFailure10(int someValue)
+        {
+            var result2 = someValue
+                            .Validate(x => x <= 10, "Value cant be less than or equal 10")
+                            .Validate(x => x <= 15, "Value cant be less than or equal 15")
+                            .Validate(x => x <= 20, "Value cant be less than or equal 20");
+
+            result2.IsFailure().Should().BeTrue();
+            result2.Messages.Should().Contain($"Value cant be less than or equal {someValue}");
+        }
+
+        [Theory]
+        [InlineData(10)]
+        [InlineData(15)]
+        [InlineData(20)]
+        [InlineData(21)]
+        public void WhenNestedValidatePasslThenContinueChain(int someValue)
+        {
+            var result2 = someValue
+                            .Validate(x => x <= 10, "Value cant be less than or equal 10")
+                            .Validate(x => x <= 15, "Value cant be less than or equal 15")
+                            .Validate(x => x <= 20, "Value cant be less than or equal 20")
+                            .Then(x => x * 2);
+
+            if(someValue < 21)
+            {
+                result2.IsFailure().Should().BeTrue();
+                result2.Messages.Should().Contain($"Value cant be less than or equal {someValue}");
+            }
+            else
+            {
+                result2.IsSuccess().Should().BeTrue();
+                result2.Value.Should().Be(someValue*2);
+            }
+            
+        }
+
+        [Theory]
+        [InlineData(10)]
+        [InlineData(15)]
+        [InlineData(20)]
+        [InlineData(22)]
+        [InlineData(25)]
+        public void WhenNestedValidatePasslThenContinueThenChain(int someValue)
+        {
+            var result2 = someValue
+                            .Validate(x => x <= 10, "Value cant be less than or equal 10")
+                            .Validate(x => x <= 15, "Value cant be less than or equal 15")
+                            .Validate(x => x <= 20, "Value cant be less than or equal 20")
+                            .Is<int, string>(x => (x * 2) < 50, x => "Product is less than 50".ToResults(), x => "Product is more than 50".ToResults());
+
+            if (someValue < 21)
+            {
+                result2.IsFailure().Should().BeTrue();
+                result2.Messages.Should().Contain($"Value cant be less than or equal {someValue}");
+            }
+            else
+            {
+                result2.IsSuccess().Should().BeTrue();
+
+                if (someValue * 2 < 50)
+                {
+                    result2.Value.Should().Contain("Product is less than 50");
+                }
+                else
+                {
+                    result2.Value.Should().Contain("Product is more than 50");
+                }
+            }
+        }
+    }
+}
--- Results/Extension/ResultsExtension.cs
+++ Results/Extension/ResultsExtension.cs
@@ -52,6 +52,93 @@ public static class ResultsExtension
         return result;
     }
 
+    public static IResults<T> ToResults<T>(this T value)
+    {
+        return ResultsTo.Something<T>(value);
+    }
+
+    public static IResults<T> Is<T>(this IResults<T> results, Func<T, bool> predicate)
+    {
+        if (!results.IsSuccess())
+        {
+            return results;
+        }
+
+        if (results.Value is null)
+        {
+            return ResultsTo.Failure<T>("Value is null");
+        }
+
+        if(!predicate(results.Value))
+        {
+            return ResultsTo.Failure<T>("Predicate results to false");
+        }
+
+        return results;
+    }
+
+    public static IResults<T2> Is<T, T2>(this IResults<T> results, Func<T, bool> condition, Func<IResults<T>, IResults<T2>> then)
+    {
+        if (condition(results.Value))
+        {
+            return (IResults<T2>)then(results);
+        }
+
+        return (IResults<T2>)false.ToResults();
+    }
+
+    public static IResults<T2> Is<T, T2>(this IResults<T> results, Func<T, bool> condition, Func<IResults<T>, IResults<T2>> then, Func<IResults<T>, IResults<T2>> @else)
+    {
+        if (!results.IsSuccess())
+        {
+            return ResultsTo.Failure<T2>().FromResults(results);
+        }
+
+        if (condition(results.Value))
+        {
+            return (IResults<T2>)then(results);
+        }
+
+        return (IResults<T2>) @else(results);
+    }
+
+    public static IResults<T> Validate<T>(this IResults<T> results, Func<T, bool> predicate, string message)
+    {
+        if (!results.IsSuccess())
+        {
+            return results;
+        }
+
+        if (predicate(results.Value))
+        {
+            return ResultsTo.Failure<T>().WithMessage(message);
+        }
+
+        return ResultsTo.Success(results.Value);
+    }
+
+    public static IResults<T> Validate<T>(this T results, Func<T, bool> predicate, string message)
+    {
+        if (predicate(results))
+        {
+            return ResultsTo.Failure<T>().WithMessage(message);
+        }
+
+        return ResultsTo.Success(results);
+    }
+
+    public static IResults<T1> Then<T, T1>(this IResults<T> results, Func<T, T1> predicate)
+    {
+        if (!results.IsSuccess())
+        {
+            return ResultsTo.Failure<T1>().FromResults(results);
+        }
+
+        var result = predicate(results.Value);
+
+        return ResultsTo.Something<T1>(result);
+    }
+
     // public static IFluentResults OnFailure(this IFluentResults result, Func<IFluentResults> func)
     // {
     //     if (result is { } fluentResults && fluentResults.Status == FluentResultsStatus.Failure)
